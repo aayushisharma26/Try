cf5ad6bda8a3a39182c17c10bf52e15cfb85c516
Aayushi Sharma <aayushisharma20@navgurukul.org>
2023-11-20 15:36:58 +0530

    remove data

--- src/Add_Data.js
+++ src/Add_Data.js
@@ -2,9 +2,13 @@ function Home() {
     return (
       <div className="App1">
         <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+
+
       </div>
     )
   }
   
-  export default Home;
+  export default Home
   
